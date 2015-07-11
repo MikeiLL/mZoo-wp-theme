86b5e84890915b1c22f8fa4ad814bd0d03f83911
Mike iLL Kilmer <mike@mzoo.org>
2015-07-11 16:45:26 -0500

    Home and bootstrap nav.

---
+++ home.php
@@ -0,0 +1,41 @@
+<div class="home_container">
+	<div id="bp_foundation" class="container-fluid bp_foundation current">
+		<div class="row-fluid heading_stripe"><h2>Foundation</h2></div>
+		<p>The first thing we do is establish WHAT the goals are. <a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+	</div>
+
+	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+		<div class="row-fluid heading_stripe"><h2>Lean</h2></div>
+		<p>The first thing we do is establish WHAT the goals are. <a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+	</div>
+
+	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+		<div class="row-fluid heading_stripe"><h2>Twelve-Factor</h2></div>
+		<p>The first thing we do is establish WHAT the goals are. <a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+	</div>
+
+	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+		<div class="row-fluid heading_stripe"><h2>Green Hosting</h2></div>
+		<p>The first thing we do is establish WHAT the goals are. <a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+	</div>
+
+	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+		<div class="row-fluid heading_stripe"><h2>Open Source</h2></div>
+		<p>The first thing we do is establish WHAT the goals are. <a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+	</div>
+
+	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+		<div class="row-fluid heading_stripe"><h2>Flexible</h2></div>
+		<p>The first thing we do is establish WHAT the goals are. <a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+	</div>
+
+	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+		<div class="row-fluid heading_stripe"><h2>Sustainable</h2></div>
+		<p>The first thing we do is establish WHAT the goals are. <a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+	</div>
+</div>
+<button name="prev">go to previous div</button>
+<button name="next">go to next div</button>
+
+
+
