b2226c772fafd2444d066a7d4f93cdfe32310db5
Mike iLL Kilmer <mike@mzoo.org>
2015-07-11 18:27:59 -0500

    Begin creating the dialogue.

--- home.php
+++ home.php
@@ -1,5 +1,14 @@
-<h1 class="media-zen">Media Zen Organic Outreach Organization</h1>
+<h1 class="media-zen">Media Zen Organic Outreach . Organization</h1>
 <div class="home_container">
+
+	<div id="bp_foundation" class="container-fluid bp_foundation current">
+		<div class="row-fluid heading_stripe"><h2>The Process</h2></div>
+		<p>Many years of Web Application development have slowly cultivated a spiral process
+		by which the needs of the client are heard, clarified, developed and refined.
+		
+		<a name="next"class="btn round orange pull-right" href="#">Here's how it works >></a></p>
+	</div>
+	
 	<div id="bp_foundation" class="container-fluid bp_foundation current">
 		<div class="row-fluid heading_stripe"><h2>Foundation</h2></div>
 		<p>The first thing we do is establish WHAT the goals are. 
