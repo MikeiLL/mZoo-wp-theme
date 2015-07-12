1ba95d4d0c707c02a58c7e99a9ffa1afca7648fc
Mike iLL Kilmer <mike@mzoo.org>
2015-07-12 11:40:26 -0500

    Force footer to page bottom.

--- home.php
+++ home.php
@@ -1,113 +1,122 @@
-<h1 class="media-zen">Media Zen Organic Outreach . Organization</h1>
-<div class="home_container">
+	<h1 class="media-zen">Media Zen Organic Outreach . Organization</h1>
+	<div class="home_container">
 
-	<div id="bp_foundation" class="container-fluid bp_foundation current">
-		<div class="row-fluid heading_stripe"><h2>The Process</h2></div>
-		<p>Many years of Web Application development have slowly cultivated a spiral process
-		by which the needs of the client are heard, clarified, developed and refined.
+		<div id="bp_foundation" class="container-fluid bp_foundation current">
+			<div class="row-fluid heading_stripe"><h2>Data Deposits</h2></div>
+			<p>Invest in yourself. We help you to cultivate your company's presence in the datasphere.
 		
-		<a name="next"class="btn round orange pull-right" href="#">Here's how it works >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Here's how >></a></p>
+		</div>
+
+		<div id="bp_foundation" class="container-fluid bp_foundation current">
+			<div class="row-fluid heading_stripe"><h2>The Process</h2></div>
+			<p>A spiraling cycle cultivated over years of web application development have slowly
+			by which the needs of the client are heard, clarified, developed and refined.
+		
+			<a name="next"class="btn round orange pull-right" href="#">See how it works >></a></p>
+		</div>
 	
-	<div id="bp_foundation" class="container-fluid bp_foundation current">
-		<div class="row-fluid heading_stripe"><h2>Foundation</h2></div>
-		<p>The first thing we do is establish <em>what</em> the goals are. We determine project 
-		requirements, desires and ultimate vision.
+		<div id="bp_foundation" class="container-fluid bp_foundation current">
+			<div class="row-fluid heading_stripe"><h2>Foundation</h2></div>
+			<p>The first thing we do is establish <em>what</em> the goals are. We determine project 
+			requirements, desires and ultimate vision.
 		
-		<a name="next"class="btn round orange pull-right" href="#">Then what >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Then what >></a></p>
+		</div>
 	
-	<div id="bp_foundation" class="container-fluid bp_foundation current">
-		<div class="row-fluid heading_stripe"><h2>Scaleable</h2></div>
-		<p>Most projects are at least <em>somewhat</em> budget-constrained, so we develop a scaleable
-		strategy, starting by developing the most important elements. The project is structured 
-		so that additional features can be added later on.
+		<div id="bp_foundation" class="container-fluid bp_foundation current">
+			<div class="row-fluid heading_stripe"><h2>Scaleable</h2></div>
+			<p>Most projects are at least <em>somewhat</em> budget-constrained, so we develop a scaleable
+			strategy, starting by developing the most important elements. The project is structured 
+			so that additional features can be added later on.
 		
-		<a name="next"class="btn round orange pull-right" href="#">More >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">More >></a></p>
+		</div>
 	
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Lasting</h2></div>
-		<p>Use frameworks, languages and environments that have stood the test of
-		software-time (however brief) so that the foundation will (be more likely to)
-		remain relevant in years to come. 
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Lasting</h2></div>
+			<p>Use frameworks, languages and environments that have stood the test of
+			software-time (however brief) so that the foundation will (be more likely to)
+			remain relevant in years to come. 
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 	
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Reuse</h2></div>
-		<p>Usually the functionality we need has already been invented. All we need to do is 
-		select the best version to meet our needs and implement it within the Application Framework. 
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Reuse</h2></div>
+			<p>Usually the functionality we need has already been invented. All we need to do is 
+			select the best version to meet our needs and implement it within the Application Framework. 
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
-
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Lean</h2></div>
-		<p>Ever notice a website that takes forever to load the first time? Often half of the 
-		code being loaded isn't even necessary to the functionality of the specific site. 
-		This can be avoided by compiling <em>only</em> the necessary components into small
-		files. 
-		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 	
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Source Control</h2></div>
-		<p>Cloud-hosted source/version control records changes to code files
-		 over time so that specific versions can be recalled and referenced later. 
-		 Development can occur while maintaing a working application.
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Open Source</h2></div>
+			<p>Benefit from the millions of ours of international development time
+			that continue to develop a host of popular libraries and frameworks
+			that can meet almost any requirement.
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
+
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Lean</h2></div>
+			<p>Ever notice a website that takes forever to load the first time? Often half of the 
+			code being loaded isn't even necessary to the functionality of the specific site. 
+			This can be avoided by compiling <em>only</em> the necessary components into small
+			files. 
+		
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 	
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Documentation</h2></div>
-		<p>Invest now in future development time by writing clear, well documented
-		code to take the guess work and sleuthing out of future maintenance, 
-		enhancement and refining.
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Source Control</h2></div>
+			<p>Cloud-hosted source/version control records changes to code files
+			 over time so that specific versions can be recalled and referenced later. 
+			 Development can occur while maintaing a working application.
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
+	
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Documentation</h2></div>
+			<p>Invest now in future development time by writing clear, well documented
+			code to take the guess work and sleuthing out of future maintenance, 
+			enhancement and refining.
+		
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Twelve-Factor</h2></div>
-		<p>The first thing we do is establish WHAT the goals are. 
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Twelve-Factor</h2></div>
+			<p>The first thing we do is establish WHAT the goals are. 
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Green Hosting</h2></div>
-		<p>Our servers use 27% less power than the average server because they 
-		contain energy efficient components. A portion of hosting proceeds are donated to 
-		The Woodland Trust (TWT).
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Green Hosting</h2></div>
+			<p>Our servers use 27% less power than the average server because they 
+			contain energy efficient components. A portion of hosting proceeds are donated to 
+			The Woodland Trust (TWT).
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Open Source</h2></div>
-		<p>The first thing we do is establish WHAT the goals are. 
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Flexible</h2></div>
+			<p>The first thing we do is establish WHAT the goals are. 
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Flexible</h2></div>
-		<p>The first thing we do is establish WHAT the goals are. 
+		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
+			<div class="row-fluid heading_stripe"><h2>Sustainable</h2></div>
+			<p>The first thing we do is establish WHAT the goals are. 
 		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
+		</div>
 	</div>
-
-	<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
-		<div class="row-fluid heading_stripe"><h2>Sustainable</h2></div>
-		<p>The first thing we do is establish WHAT the goals are. 
-		
-		<a name="next"class="btn round orange pull-right" href="#">Next >></a></p>
-	</div>
-</div>
 
 
 
