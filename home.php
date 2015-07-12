b8797498169e360987ead4575aa5e664b43e56cb
Mike iLL Kilmer <mike@mzoo.org>
2015-07-12 17:19:25 -0500

    Absolute position button.

--- home.php
+++ home.php
@@ -10,16 +10,16 @@
 					<li>developed</li>
 					<li>refined</li>
 				</ul>
-		
-			<a name="next"class="btn round pink pull-right" href="#">See how it works >></a></p>
+			</p>
+			<a name="next"class="btn round pink pull-right" href="#">See how it works >></a>
 		</div>
 	
 		<div id="bp_foundation" class="container-fluid bp_foundation current">
 			<div class="row-fluid heading_stripe"><h2>First steps</h2></div>
 			<p>The first thing we do is establish <em>what</em> the goals are. 
-			We determine project requirements, desires and ultimate vision.
+			We determine project requirements, desires and ultimate vision.</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Then what >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Then what >></a>
 		</div>
 		
 		<div id="bp_foundation" class="container-fluid bp_foundation current">
@@ -27,8 +27,9 @@
 			<p>Identify the entity and cultivate a presence in the datasphere by 
 			archiving and sharing useful information in three categories: fun, 
 			informative and introspective.
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Read more >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Read more >></a>
 		</div>
 	
 		<div id="bp_foundation" class="container-fluid bp_foundation current">
@@ -36,8 +37,9 @@
 			<p>Most projects are at least <em>somewhat</em> budget-constrained, so we develop a scaleable
 			strategy, starting by developing the most important elements. The project is structured 
 			so that additional features can be added later on.
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">More >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">More >></a>
 		</div>
 	
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
@@ -45,16 +47,19 @@
 			<p>Use frameworks, languages and environments that have stood the test of
 			software-time (however brief) so that the foundation will (be more likely to)
 			remain relevant in years to come. 
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 	
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
 			<div class="row-fluid heading_stripe"><h2>Reuse</h2></div>
 			<p>Usually the functionality we need has already been invented. All we need to do is 
-			select the best version to meet our needs and implement it within the Application Framework. 
+			select the best version to meet our needs and implement it within the Application 
+			Framework. 
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 	
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
@@ -62,8 +67,9 @@
 			<p>Benefit from thousands of hours of international development time
 			that continue to develop a host of freely shared popular libraries and frameworks
 			that can meet almost any requirement.
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
@@ -72,8 +78,9 @@
 			code being loaded isn't even necessary to the functionality of the specific site. 
 			This can be avoided by compiling <em>only</em> the necessary components into small
 			files. 
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 	
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
@@ -81,8 +88,9 @@
 			<p>Cloud-hosted source/version control records changes to code files
 			 over time so that specific versions can be recalled and referenced later. 
 			 Development can occur while maintaing a working application.
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 	
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
@@ -90,15 +98,17 @@
 			<p>Invest now in future development time by writing clear, well documented
 			code to take the guess work and sleuthing out of future maintenance, 
 			enhancement and refining.
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
 			<div class="row-fluid heading_stripe"><h2>Twelve-Factor</h2></div>
 			<p>The first thing we do is establish WHAT the goals are. 
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
@@ -106,22 +116,25 @@
 			<p>Our servers use 27% less power than the average server because they 
 			contain energy efficient components. A portion of hosting proceeds are donated to 
 			The Woodland Trust (TWT).
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
 			<div class="row-fluid heading_stripe"><h2>Flexible</h2></div>
 			<p>The first thing we do is establish WHAT the goals are. 
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 
 		<div id="bp_foundation" class="container-fluid bp_foundation" style="display:none">
 			<div class="row-fluid heading_stripe"><h2>Sustainable</h2></div>
 			<p>The first thing we do is establish WHAT the goals are. 
+			</p>
 		
-			<a name="next"class="btn round pink pull-right" href="#">Next >></a></p>
+			<a name="next"class="btn round pink pull-right" href="#">Next >></a>
 		</div>
 	</div>
 
