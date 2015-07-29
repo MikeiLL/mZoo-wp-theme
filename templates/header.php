e9dd7db47f45041b8d8827f4c63719d7a4b95c74
Mike iLL Kilmer <mike@mzoo.org>
2015-07-28 22:22:30 -0500

    Admin bar on phone conflicting with nav bar.

--- templates/header.php
+++ templates/header.php
@@ -2,28 +2,25 @@
   // This file assumes that you have included the nav walker from https://github.com/twittem/wp-bootstrap-navwalker
   // somewhere in your theme.
 ?>
-
 	<header class="banner navbar navbar-default navbar-static-top" role="banner">
 	  <div class="container">
 		<div class="navbar-header">
-		<?php
-		if ((has_nav_menu('primary_navigation')) && !(is_home() || is_front_page())) : ?>
 		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
 			<span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 		  </button>
-		<?php endif; ?>
+
 		  <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
 		</div>
 		
 		<nav class="collapse navbar-collapse" role="navigation">
 		  <?php
-		  if ((has_nav_menu('primary_navigation')) && !(is_home() || is_front_page())) :
+		  if ((has_nav_menu('primary_navigation')) && !(is_front_page())) :
 			wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
 		  endif;
 		  ?>
 		</nav>
 	  </div>
-	</header>
+	</header> 
