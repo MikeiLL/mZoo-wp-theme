f45f852bad12a79b99bb47c8e8309e679e6fe88c
Mike iLL Kilmer <mike@mzoo.org>
2015-07-28 21:06:17 -0500

    Remove dev copy. Home did not act as had hoped.

--- index.php
+++ index.php
@@ -14,6 +14,6 @@ $zoo_loop = new WP_Query( $args );
 <?php while ($zoo_loop->have_posts()) : $zoo_loop->the_post(); ?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
-<h1>Hi</h1>
+
 <?php the_posts_navigation(); ?>
 
