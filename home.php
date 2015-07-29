7fae2a917e3f828fb6d396cbe2190f0230fd00cb
Mike iLL Kilmer <mike@mzoo.org>
2015-07-28 20:58:22 -0500

    Move journal tamplate file here.

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Journal Template
+ */
+?>
+
+<?php get_template_part('templates/page', 'header'); ?>
+<h1>Hi</h1>
+<?php if (!have_posts()) : ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'sage'); ?>
+  </div>
+  <?php get_search_form(); ?>
+<?php endif; ?>
+<?php
+$args = array( 'post_type' => 'post', 'posts_per_page' => 10 );
+$zoo_loop = new WP_Query( $args );
+?>
+
+<?php while ($zoo_loop->have_posts()) : $zoo_loop->the_post(); ?>
+  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+  <?php echo get_post_type() ?>
+<?php endwhile; ?>
+
+<?php the_posts_navigation(); ?>
+
+
