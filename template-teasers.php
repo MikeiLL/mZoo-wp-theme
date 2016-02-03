64e6b16b260791fd7d7a007f7660fd24473d1efc
Mike iLL Kilmer <mike@mzoo.org>
2016-02-03 12:44:30 -0600

    Include the_content in teaser template.

--- template-teasers.php
+++ template-teasers.php
@@ -6,15 +6,21 @@
 	<h1 class="media-zen">
 		<?php echo get_bloginfo ( 'description' ); ?>
 	</h1>
+
+  <div class="row-fluid">
+    <?php while (have_posts()) : the_post(); ?>
+    <?php the_content(); ?>
+    <?php endwhile; ?>
+  </div>
 	<div class="home_container">
-	
+
 		<?php
 		// The Query
 		$teaser_query = new WP_Query( 'post_type=teasers' );
 
 		?>
 
-		<?php while ($teaser_query->have_posts()) : $teaser_query->the_post(); ?>  
+		<?php while ($teaser_query->have_posts()) : $teaser_query->the_post(); ?>
 			<div id="bp_foundation" class="container-fluid bp_foundation current">
 				<div class="row-fluid heading_stripe">
 					<?php the_title( '<h2>', '</h2>' ); ?>
@@ -23,14 +29,14 @@
 				<a name="next" class="btn round pink pull-right" href="#">See how it works >></a>
 			</div>
 		<?php endwhile; ?>
-		<?php 
+		<?php
 		/* Restore original Post Data */
-		wp_reset_postdata(); 
+		wp_reset_postdata();
 		?>
 
-		
+
 	</div>
-		
+
 
 
 
